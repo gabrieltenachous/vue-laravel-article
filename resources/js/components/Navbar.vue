8ad497abaa46c7057d1a048bb270cb4d1baf0bfc
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2022-07-11 11:23:49 -0300

    =

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="navbar navbar-exand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand">
+                Larticle
+            </a>
+        </div>
+    </nav>
+</template>
